7a40e5322ec8e8794effc25964326eaa14077d35
pelumi <ppeliance@gmail.com>
2025-06-29 22:51:00 +0100

    new

--- frontend/src/component/searchInput.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-"use client";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SearchInput = ({ value, onChange }) => (<div className="px-5 pb-5">
-    <input className="w-full px-4 py-3 bg-tertiary-bg border text-primary border-primary mt-3 h-[50px] rounded text-sm focus:outline-none focus:border-[#58a6ff] focus:ring-4 focus:ring-[#58a6ff]/20" type="text" value={value} onChange={(e) => onChange(e.target.value)} placeholder="Search friends by name or email"/>
-  </div>);
-exports.default = SearchInput;
