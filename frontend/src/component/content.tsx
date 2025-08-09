9da04677f03fea0e5e26e83c4b91a55146d3e26b
ppeliance <144333180+ppeliance@users.noreply.github.com>
2025-08-09 11:16:38 +0100

    Update content.tsx

--- frontend/src/component/content.tsx
+++ frontend/src/component/content.tsx
@@ -31,7 +31,7 @@ const Content: React.FC<ContentProps> = ({  }) => {
   };
 
   return (
-  <section id="home" className="relative min-h-screen flex items-center justify-center text-center px-[5%] bg-[radial-gradient(ellipse_at_center,_rgba(88,166,255,0.1)_0%,_transparent_50%)] overflow-hidden">
+  <section id="home" className="relative  min-h-screen flex items-center justify-center text-center px-[5%] bg-[radial-gradient(ellipse_at_center,_rgba(88,166,255,0.1)_0%,_transparent_50%)] overflow-hidden">
   {/* Grid Overlay */}
   <div className="absolute inset-0 opacity-30 z-0 pointer-events-none bg-[url('data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22%3E%3Cdefs%3E%3Cpattern id=%22grid%22 width=%2210%22 height=%2210%22 patternUnits=%22userSpaceOnUse%22%3E%3Cpath d=%22M 10 0 L 0 0 0 10%22 fill=%22none%22 stroke=%22%23ffffff%22 stroke-width=%220.5%22 opacity=%220.1%22/%3E%3C/pattern%3E%3C/defs%3E%3Crect width=%22100%22 height=%22100%22 fill=%22url(%23grid)%22/%3E%3C/svg%3E')]">
   </div>
